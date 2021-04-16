c1334e97bb6c324a1306665ba9dfec31efc20041
左奇银 <zuoqiyin@foxmail.com>
2021-04-16 22:17:59 +0800

    增加项目所使需sql

---
+++ sql/db2021.sql
@@ -0,0 +1,7 @@
+-- 创建payment
+CREATE TABLE `payment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `serial` varchar(200) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
